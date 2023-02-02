90c3d48c1f92d6f62888ea19a00f9a09f831c6f0
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-02-02 08:45:38 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = 8000;
+const DB_URL = 'mongodb+srv://EduMor:EduMor2k23@edumor-lms.1zyz2xw.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+});
+
+mongoose.connect(DB_URL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => console.log('DB connection eroor', err));
+
+app.listen(PORT, () => {
+    console.log(`Server started port on ${PORT}`);
+});
