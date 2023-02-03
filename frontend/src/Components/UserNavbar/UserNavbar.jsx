c13ba15581873f7639077a8b65e46ed11c9f7a9c
ChaviruAtthanayake <86518217+ChaviruAtthanayake@users.noreply.github.com>
2023-02-03 22:17:38 +0530

    Added changes

--- frontend/src/Components/UserNavbar/UserNavbar.jsx
+++ frontend/src/Components/UserNavbar/UserNavbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./UserNavbar.css";
+import Logo from "../../Assets/LogoC.png";
 
 function UserNavbar() {
   return (
-    <div>
-      asd
+    <div className="login-page">
+      <div className="navbar">
+        <div className="logo">
+          <img className="logo-img" src={Logo} />
+          <span className="text">EduMor</span>
+        </div>
+        <div className="menu">
+          <ul>
+            <li>Features</li>
+            <li>Examinations</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <button type="button" value="signup" className="button">
+          SignUp
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UserNavbar
+export default UserNavbar;
--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import UserNavbar from '../../Components/UserNavbar/UserNavbar'
+import LoginForm from './LoginForm/LoginForm'
 
 function Login() {
   return (
     <div>
       <UserNavbar/>
+      <LoginForm/>
     </div>
   )
 }
