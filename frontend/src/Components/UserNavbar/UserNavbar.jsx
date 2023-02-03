6d66f803d7ced33caf7af8ed46d1720351153431
ChaviruAtthanayake <86518217+ChaviruAtthanayake@users.noreply.github.com>
2023-02-03 22:16:51 +0530

    Added relevant changes and added the login page

---
+++ frontend/src/Components/UserNavbar/UserNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserNavbar() {
+  return (
+    <div>
+      asd
+    </div>
+  )
+}
+
+export default UserNavbar
---
+++ frontend/src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserNavbar from '../../Components/UserNavbar/UserNavbar'
+
+function Login() {
+  return (
+    <div>
+      <UserNavbar/>
+    </div>
+  )
+}
+
+export default Login
