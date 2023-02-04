959aca435153d387fdcd2434daa4a5ecc26f983d
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-02-04 21:50:53 +0530

    added vacancies page

---
+++ frontend/src/Pages/Vacancy/Vacancies.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import SideBar from '../../Components/SideBar/SideBar';
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
+import { MDBBreadcrumb, MDBBreadcrumbItem } from 'mdb-react-ui-kit';
+
+function Vacancies() {
+    const { t } = useTranslation();
+  return (
+    <div style={{ diplay: 'flex', flexDirection: 'column' }}>
+      <div className="middle-contaier" style={{ display: "flex" }}>
+        <SideBar />
+        <div className="mainContainer">
+          <Navbar />
+          <div className="read-crumb">
+            <MDBBreadcrumb >
+              <MDBBreadcrumbItem>
+                <a href='/'>{t("Overview")}</a>
+              </MDBBreadcrumbItem>
+              <MDBBreadcrumbItem active>{t("Vacancy")}</MDBBreadcrumbItem>
+            </MDBBreadcrumb>
+          </div>
+          <h5>{t("Vacancy")}</h5>
+          <section >
+           
+          </section>
+        </div>
+      </div>
+      <div className="footer" style={{ diplay: 'flex' }}>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Vacancies
