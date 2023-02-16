987ac9128f80f71af1798b422628a2638f81f47b
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-02-16 19:41:21 +0530

    added super admin pages

--- frontend/src/Pages/SuperAdmin/AdminRequests/AdminRequests.jsx
+++ frontend/src/Pages/SuperAdmin/AdminRequests/AdminRequests.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import SASidebar from '../../../Components/SuperAdmin/SASidebar/SASidebar'
+import SANavbar from '../../../Components/SuperAdmin/SANavbar/SANavbar'
 import './AdminRequests.css'
+import Footer from '../../../Components/Footer/Footer'
 
 function AdminRequests() {
   return (
-    <div>AdminRequests</div>
+    <div style={{ diplay: "flex", flexDirection: "column" }}>
+    <div className="overview" style={{ display: "flex" }}>
+      <SASidebar />
+      <div className="mainContainer">
+        <SANavbar />
+        <div className="overview-container" style={{ display: "flex" }}>
+          
+        </div>
+      </div>
+    </div>
+    <div className="footer" style={{ diplay: "flex" }}>
+      <Footer />
+    </div>
+  </div>
   )
 }
 
