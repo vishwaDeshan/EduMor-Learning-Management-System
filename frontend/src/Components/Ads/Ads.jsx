06fd89b03a176ac47e44b62797d8fb5a5cf2c2bf
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-02-16 12:16:52 +0530

    retrieve data from advertisements

--- frontend/src/Components/Ads/Ads.jsx
+++ frontend/src/Components/Ads/Ads.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ImageSlideshow from './ImageSlideshow';
-
-const images = [
- 
-  'https://d3jmn01ri1fzgl.cloudfront.net/photoadking/webp_thumbnail/5fbf8993831f9_template_image_1606388115.webp',
-  'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/online-education-banner-ad-template-design-0c4dd789976fba20292b75f1a3f099d8_screen.jpg?ts=1619075849',
-  'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/study-abroad-modern-creative-education-advert-design-template-fe65662c2b23d374c79001d1804202e4_screen.jpg?ts=1641159132',
-  'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/higher-education-banner-ad-template-design-3fd8722fcc1fce675adee477e9fcf169_screen.jpg?ts=1621843628',
-  'https://pbs.twimg.com/media/ESQ0av7XYAINLKl.jpg',
-  'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/study-abroad-advertisement-instagram-post-ban-design-template-e22121e5a57a5c7e26108a7d0ed92a7a_screen.jpg?ts=1623779720',
-  'https://pbs.twimg.com/media/FaWOkSvVsAAjIMR?format=jpg&name=large'
-]
+import axios from 'axios';
 
 const Ads = () => {
+  const [ads, setAds] = useState([]);
+  useEffect(() => {
+    function getAds() {
+      axios.get('http://localhost:8000/advertisements').then((res) => {
+        console.log(res.data.existingAds);
+        setAds(res.data.existingAds)
+      }).catch((err) => {
+        alert(err.message)
+      })
+    }
+    getAds();
+  }, [])
+
   return (
     <div>
-      <ImageSlideshow images={images} />
+      <a href={ads.map(ad => ad.link)} target="_blank" rel="noopener noreferrer">
+        <ImageSlideshow images={ads.map(ad => ad.image)} />
+      </a>
     </div>
   );
 };
