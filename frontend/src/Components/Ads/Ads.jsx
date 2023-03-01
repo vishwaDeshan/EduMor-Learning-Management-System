c62e7eab53cf1cc272212bcab1f803a4eeeed467
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-03-01 21:33:45 +0530

    Edited conflict on ads.jsx

--- frontend/src/Components/Ads/Ads.jsx
+++ frontend/src/Components/Ads/Ads.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import ImageSlideshow from './ImageSlideshow';
-import React from 'react'
 import './Ads.css'
 import Ads1 from '../../Assets/Ads1.png'
 import axios from 'axios';
@@ -20,12 +19,12 @@ const Ads = () => {
   }, [])
 
 
-function Ads() {
-  return (
-    <div className="ads">
-      <img src={Ads1} alt="Ads" srcset=""/>
-    </div>
-  )
+  function Ads() {
+    return (
+      <div className="ads">
+        <img src={Ads1} alt="Ads" srcset="" />
+      </div>
+    )
+  }
 }
-
 export default Ads
