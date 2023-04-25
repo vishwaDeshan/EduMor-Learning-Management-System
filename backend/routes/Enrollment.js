5086453a7a0f52e8b1f86415b5ae91baa00a4384
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-04-25 15:57:37 +0530

    added middlware

--- backend/routes/Enrollment.js
+++ backend/routes/Enrollment.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../Middlewares/authMiddleware');
 const enrollmentController = require('../Controllers/enrollmentController');
 
 // Route for enrolling a user in an examination
